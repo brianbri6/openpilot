3515b12988a093f8476625b8d74e4f5a8ac5c205
Brian <brianbri6@hotmail.com>
2020-12-14 21:14:31 -0500

    Update launch_chffrplus.sh

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -21,15 +21,15 @@ function launch {
   wpa_cli IFNAME=wlan0 SCAN
 
   # apply update only if no_ota_updates does not exist in /data directory
-  file="/data/no_ota_updates"
-  if ! [ -f "$file" ]; then
-    if [ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]; then
+  #file="/data/no_ota_updates"
+  #if ! [ -f "$file" ]; then
+  #  if [ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]; then
       git reset --hard @{u} &&
       git clean -xdf &&
       
-      exec "${BASH_SOURCE[0]}"
-    fi
-  fi
+   #   exec "${BASH_SOURCE[0]}"
+  #  fi
+ #fi
 
   # no cpu rationing for now
   echo 0-3 > /dev/cpuset/background/cpus
@@ -42,16 +42,15 @@ function launch {
 
   # Remove old NEOS update file
   # TODO: move this code to the updater
-  if [ -d /data/neoupdate ]; then
-    rm -rf /data/neoupdate
-  fi
+ # if [ -d /data/neoupdate ]; then
+  #  rm -rf /data/neoupdate
+ #fi
 
   # Check for NEOS update
  # if [ $(< /VERSION) != "14" ]; then
   #  if [ -f "$DIR/scripts/continue.sh" ]; then
    #   cp "$DIR/scripts/continue.sh" "/data/data/com.termux/files/continue.sh"
   #  fi
-   #  cp "$DIR/scripts/continue.sh" "/data/data/com.termux/files/continue.sh"
     #"$DIR/installer/updater/updater" "file://$DIR/installer/updater/update.json"
   #fi
 
